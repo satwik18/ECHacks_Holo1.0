9007aae4a8ab0ea5e1324c55d8c119a5247c21b0
satwik18 <satwik18@gmail.com>
2018-11-04 10:01:49 -0500

    Finished Project Commit

---
+++ Hologram_1.0/Assets/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	public void Scene_Change (string sceneName) {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(name: "WorkingScene_2");
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
